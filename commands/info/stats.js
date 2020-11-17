d3947b00c357804b21d943622a2bb4dda0b4af57
Sanat <68254785+sanspyer@users.noreply.github.com>
2020-11-16 21:20:20 -0500

    Update stats.js

--- commands/info/stats.js
+++ commands/info/stats.js
@@ -12,7 +12,7 @@ module.exports = {
         const seconds = Math.round(uptime % 60);
         const botuptime = (days > 0 ? days + " days, ":"") + (hours > 0 ? hours + " hours, ":"") + (minutes > 0 ? minutes + " minutes, ":"") + (seconds > 0 ? seconds + " seconds":"")
         message.channel.send(new MessageEmbed()
-        .setAuthor(client.user.username, client.user.displayAvatarURL({ dynamic: true }), "https://antialt.xyz/")
+        .setAuthor(client.user.username, client.user.displayAvatarURL({ dynamic: true }),)
         .setColor('RANDOM')
         .setThumbnail(client.user.displayAvatarURL({ dynamic: true }))
         .setDescription(` • **Uptime:** \`${botuptime}\`\n • **Ping:** \`${Math.floor(client.ws.ping)}ms\`\n\n • **Servers:** \`${client.guilds.cache.size}\`\n • **Users:** \`${eval(client.guilds.cache.map(g => g.members.cache.filter(m => !m.user.bot).size).join(' + '))}\`\n •**Bots:** \`${eval(client.guilds.cache.map(g => g.members.cache.filter(m => m.user.bot).size).join(' + '))}\`\n •**Channels:** \`${client.channels.cache.size}\`\n • **Emojis:** \`${client.emojis.cache.size}\``)
